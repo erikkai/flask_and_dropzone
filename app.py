3f31bd00da633d4edb0734d40751b579626417a3
Erikka <19295862+erikkai@users.noreply.github.com>
2021-06-27 02:21:46 -0700

    Code for backend

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for, abort
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+app.config['UPLOAD_EXTENSIONS'] = ['.mov', '.mp4', '.m4v', '.jpm', '.jp2', '.3gp', '.3g2', '.mkv', '.mpg', '.ogv', '.webm', '.wmv' ]
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    my_files = request.files
+    formData = request.form['myFormData']
+    
+    with open("my_data.txt", "x+") as my_data:
+        my_data.write(formData)
+
+    for item in my_files:
+        uploaded_file = my_files.get(item)
+        uploaded_file.filename = secure_filename(uploaded_file.filename)
+        
+        if uploaded_file.filename != '':
+            file_ext = os.path.splitext(uploaded_file.filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+
+        uploaded_file.save(uploaded_file.filename)
+
+    return redirect(url_for('index'))
